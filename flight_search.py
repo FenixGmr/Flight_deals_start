3f318a4077c675accff9a7b3e79fb86ca698bc68
FenixGmr <97828461+FenixGmr@users.noreply.github.com>
2022-06-30 23:42:20 +0300

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,11 @@
+import requests
+
+TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
+TEQUILA_API_KEY = "API_KEY"
+
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+
+    def get_destination_code(self, city_name):
+        code = "TESTING"
+        return code
